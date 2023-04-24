b3b8d9d7ad9c3413172bfacbcdde9a2b2202e875
Vera <vera.krapivner@iths.se>
2023-04-24 12:38:28 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import HomePage from './componets/HomePage/HomePage'
-import Login from './componets/Login/Login';
+
 
 function App() {
   
@@ -9,10 +9,7 @@ function App() {
   return (
     <>
     <HomePage/>
-    <article className="buttonBox">
-        <Login>Logga In</Login>
-      </article>
-  
+    
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
-import HomePage from './components/HomePage/HomePage.jsx'
-// import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// const loggedIn = false;
-// const router = createBrowserRouter(
-//   [ 
-//     {
-//       path: "/",
-//       // element:<App/>
-//       element: loggedIn ? ( <HomePage />) : (<Login onLogin={handleLogin} />)
-//     }
-
-//   ]
-// );
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-
-//   <RouterProvider router={router}/>
-
-// )
-const loggedIn = false;
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={loggedIn ? HomePage : Login} />
-      <Route path="/app" component={App} />
-    </Switch>
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
